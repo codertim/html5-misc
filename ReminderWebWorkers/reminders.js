98a2840c32dfcdb322992e622b3ff35f50bdeb7f
Tim Force <tim.w.force@gmail.com>
2014-11-11 22:26:42 -0800

    cleaned up some debugging

--- ReminderWebWorkers/reminders.js
+++ ReminderWebWorkers/reminders.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function() {
-    alert("jquery starting ...");
+    // alert("jquery starting ...");
 
     if(typeof Worker == "undefined") {
         alert("Web Workers not define!!!");
@@ -9,13 +9,15 @@ $(document).ready(function() {
 
     $("#gobutton").on("click", function() {
         var reminderMessage = $("#remindermessage").val();
+        $("#remindermessage").val("");
         var reminderTime = $("#remindertime").val();
-        alert("Go button clicked! - reminderMessage = " + reminderMessage + "   reminderTime=" + reminderTime);
+        $("#remindertime").val("");
+        // alert("Go button clicked! - reminderMessage = " + reminderMessage + "   reminderTime=" + reminderTime);
         var worker = new Worker("reminder_worker.js");
         worker.postMessage(reminderMessage + "|||" + reminderTime);
 
         worker.onmessage = function(e) {
-          alert("Hello from onmessage! - message from worker = " + e.data);
+          alert("REMINDER\n\n" + e.data + "\n\n");
         }
     });
 });
